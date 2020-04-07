fa08695d900d4f780dfc40e4055d02a6bc7306ef
Lars Michael Kristensen <lmkr@hvl.no>
2020-04-07 22:33:00 +0200

    Moved transport layer output into base unit test

--- src/no/hvl/dat110/nfr/examples/RoutingTestBase.java
+++ src/no/hvl/dat110/nfr/examples/RoutingTestBase.java
@@ -2,6 +2,8 @@ package no.hvl.dat110.nfr.examples;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import no.hvl.dat110.nrf.common.LogLevel;
+import no.hvl.dat110.nrf.common.Logger;
 import no.hvl.dat110.nrf.network.Host;
 
 public class RoutingTestBase {
@@ -10,6 +12,7 @@ public class RoutingTestBase {
 		
 		Segment segment12 = new Segment(n1.getName() + "->" + n2.getName() + " melding");
 		
+		Logger.log(LogLevel.UDT,n1.getName() + "[udt_send]:" + segment12.toString() + "," + n2.getIPAddress().toString()+ "]");
 		n1.udt_send(segment12.getBytes(), n2.getIPAddress());
 		
 		try {
@@ -24,6 +27,7 @@ public class RoutingTestBase {
 		}
 		
 		Segment segment = new Segment (new String(n2.udt_recv()));
+		Logger.log(LogLevel.UDT,n2.getName() + "[udt_recv]:" + segment.toString() + "," + n2.getIPAddress().toString()+ "]");
 		
 		assertEquals(segment12.getPayload(),segment.getPayload());
 	}
